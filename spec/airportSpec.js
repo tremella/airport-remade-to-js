2ba6fafa124b2aa302d25d39b0687539a92ba067
YohannTisserand <tisserand.yohann@gmail.com>
2021-05-05 17:46:30 +0100

    airport allowed to takeoff (more or less)

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -1,12 +1,16 @@
 'use strict';
 
 describe('Airport', () => {
-  let airport = new Airport;
-  let plane = new Plane;
+  let airport;
+  let plane;
+  beforeEach(() => {
+    airport = new Airport();
+    plane = new Plane();
+  });
 
-  // it('should be empty before landing', () => {
-  //   expect(hangar).toBe
-  // });
+  it('should be empty before landing', () => {
+    expect(airport.hangar).toEqual([])
+  });
 
   it('allows a plane to land', () => {
     airport.land(plane)
--- spec/planeSpec.js
+++ spec/planeSpec.js
@@ -1,9 +1,9 @@
-'use strict';
+// 'use strict';
 
-describe('Plane', () => {
-  let plane = Plane.new;
+// describe('Plane', () => {
+//   let plane = Plane.new;
 
-  it('should be able to land', () => {
-    
-  });
-});
+//   it('should be able to land', () => {
+
+//   });
+// });
